5c8e1e62c7c6dae32915d916761ce17a2ca4ff9c
Frank Dedden <dev@dedden.net>
2019-02-05 13:39:09 +0100

    Implemented translation of expressions

--- src/Language/C99/Simple/Translate.hs
+++ src/Language/C99/Simple/Translate.hs
@@ -2,16 +2,15 @@ module Language.C99.Simple.Translate where
 
 import GHC.Exts             (fromList)
 import Control.Monad.State  (State, execState, get, put)
-import Data.Maybe           (fromJust)
 
 import           Language.C99.Simple.AST
 import qualified Language.C99.AST         as C
 
 import Language.C99.Util.Expr
 import Language.C99.Util.IsList
+import Language.C99.Util.Wrap
 
 import Language.C99.Simple.Util
-
 
 translate = transtransunit
 
@@ -23,9 +22,6 @@ transfundef = undefined -- TODO
 
 transdecln :: Decln -> C.Decln
 transdecln (Decln storespec ty name init) = undefined
-
-transexpr :: Expr -> C.AssignExpr
-transexpr _ = undefined -- TODO
 
 getdeclr :: Type -> State C.Declr ()
 getdeclr ty = case ty of
@@ -43,7 +39,7 @@ getdeclr ty = case ty of
     put $ insertptr (C.PtrBase quals) declr
 
   Array ty' len -> do
-    let lenexpr = transexpr <$> len
+    let lenexpr = (wrap.transexpr) <$> len
     (C.Declr ptr declr) <- get
     put $ C.Declr ptr (C.DirectDeclrArray1 declr Nothing lenexpr)
 
@@ -115,3 +111,112 @@ spec2spec ts = case ts of
 
 
 transstructdecln = undefined -- TODO
+
+
+
+transexpr :: Expr -> C.Expr
+transexpr e = case e of
+  Ident     i         -> wrap $ C.PrimIdent $ ident i
+  LitInt    i         -> wrap $ litint    i
+  LitDouble d         -> wrap $ litdouble d
+  LitString s         -> wrap $ litstring s
+  Index     arr idx   -> wrap $ indexexpr arr idx
+  Funcall   fun args  -> wrap $ funcall   fun args
+  Dot       e   field -> wrap $ dotexpr   e field
+  Arrow     e   field -> wrap $ arrowexpr e field
+  InitVal   ty  init  -> wrap $ initexpr  ty init
+  UnaryOp   op e      -> wrap $ unaryop op e
+  Cast      ty e      -> wrap $ castexpr ty e
+  BinaryOp  op e1 e2  -> binaryop op e1 e2
+  AssignOp  op e1 e2  -> wrap $ assignop op e1 e2
+
+
+unaryop :: UnaryOp -> Expr -> C.UnaryExpr
+unaryop op e = case op of
+    Inc     -> C.UnaryInc          (wrap e')
+    Dec     -> C.UnaryDec          (wrap e')
+    Ref     -> C.UnaryOp C.UORef   (wrap e')
+    DeRef   -> C.UnaryOp C.UODeref (wrap e')
+    Plus    -> C.UnaryOp C.UOPlus  (wrap e')
+    Min     -> C.UnaryOp C.UOMin   (wrap e')
+    BoolNot -> C.UnaryOp C.UOBNot  (wrap e')
+    Not     -> C.UnaryOp C.UONot   (wrap e')
+  where
+    e' = transexpr e
+
+binaryop :: BinaryOp -> Expr -> Expr -> C.Expr
+binaryop op e1 e2 = case op of
+    Mult          -> wrap $ C.MultMult   (wrap e1') (wrap e2')
+    Div           -> wrap $ C.MultDiv    (wrap e1') (wrap e2')
+    Mod           -> wrap $ C.MultMod    (wrap e1') (wrap e2')
+    Add           -> wrap $ C.AddPlus    (wrap e1') (wrap e2')
+    Sub           -> wrap $ C.AddMin     (wrap e1') (wrap e2')
+    ShiftL        -> wrap $ C.ShiftLeft  (wrap e1') (wrap e2')
+    ShiftR        -> wrap $ C.ShiftRight (wrap e1') (wrap e2')
+    Lessthan      -> wrap $ C.RelLT      (wrap e1') (wrap e2')
+    GreaterThan   -> wrap $ C.RelGT      (wrap e1') (wrap e2')
+    LessThanEq    -> wrap $ C.RelLE      (wrap e1') (wrap e2')
+    GreaterThanEq -> wrap $ C.RelGE      (wrap e1') (wrap e2')
+    Equal         -> wrap $ C.EqEq       (wrap e1') (wrap e2')
+    NotEqual      -> wrap $ C.EqNEq      (wrap e1') (wrap e2')
+    And           -> wrap $ C.And        (wrap e1') (wrap e2')
+    XOr           -> wrap $ C.XOr        (wrap e1') (wrap e2')
+    Or            -> wrap $ C.Or         (wrap e1') (wrap e2')
+    LAnd          -> wrap $ C.LAnd       (wrap e1') (wrap e2')
+    LOr           -> wrap $ C.LOr        (wrap e1') (wrap e2')
+  where
+    e1' = transexpr e1
+    e2' = transexpr e2
+
+assignop :: AssignOp -> Expr -> Expr -> C.AssignExpr
+assignop op e1 e2 = C.Assign e1' op' e2' where
+  e1' = wrap $ transexpr e1
+  e2' = wrap $ transexpr e2
+  op' = case op of
+    Assign       -> C.AEq
+    AssignMult   -> C.ATimes
+    AssignDiv    -> C.ADiv
+    AssignMod    -> C.AMod
+    AssignAdd    -> C.AAdd
+    AssignSub    -> C.ASub
+    AssignShiftL -> C.AShiftL
+    AssignShiftR -> C.AShiftR
+    AssignAnd    -> C.AAnd
+    AssignXOr    -> C.AXOr
+    AssignOr     -> C.AOr
+
+transinit :: Init -> C.Init
+transinit (InitExpr e)   = C.InitExpr (wrap $ transexpr e)
+transinit (InitArray es) = C.InitArray (fromList $ map transinit es)
+
+initexpr ty init = C.PostfixInits ty' init' where
+  ty'   = transtypename ty
+  init' = fromList $ map transinit init
+
+indexexpr arr idx = C.PostfixIndex arr' idx' where
+  arr' = wrap $ transexpr arr
+  idx' = wrap $ transexpr idx
+
+dotexpr e field = C.PostfixDot e' field' where
+  e'     = wrap $ transexpr e
+  field' = ident field
+
+arrowexpr e field = C.PostfixArrow e' field' where
+  e'     = wrap $ transexpr e
+  field' = ident field
+
+castexpr ty e = C.Cast ty' e' where
+  ty' = transtypename ty
+  e'  = wrap $ transexpr e
+
+funcall fun args = C.PostfixFunction fun' args' where
+  fun'  = wrap $ transexpr fun
+  args' = Just $ fromList argses
+
+  argses :: [C.AssignExpr]
+  argses = map wrap exprs
+
+  exprs :: [C.Expr]
+  exprs = map transexpr args
+
+transtypename = undefined -- TODO
