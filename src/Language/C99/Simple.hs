4ae268f147b1bb70282cc12ba44fc10a527202b1
Frank Dedden <dev@dedden.net>
2019-02-13 15:24:36 +0100

    Export modules in Simple.hs and added Util.hs

--- src/Language/C99/Simple.hs
+++ src/Language/C99/Simple.hs
@@ -1,5 +1,9 @@
-module Language.C99.Simple where
+module Language.C99.Simple
+  ( module Language.C99.Simple.AST
+  , module Language.C99.Simple.Translate
+  , module Language.C99.Simple.Util
+  ) where
 
 import Language.C99.Simple.AST
-
 import Language.C99.Simple.Translate
+import Language.C99.Simple.Util
