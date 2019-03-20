5dcf981a05131a809d79a33aaa83d4716c698bc2
Frank Dedden <dev@dedden.net>
2019-03-20 23:05:59 +0100

    Fixed recursive call of Array getdeclr

--- src/Language/C99/Simple/Translate.hs
+++ src/Language/C99/Simple/Translate.hs
@@ -79,6 +79,7 @@ getdeclr ty = case ty of
     let lenexpr = (wrap.transexpr) <$> len
     (C.Declr ptr declr) <- get
     put $ C.Declr ptr (C.DirectDeclrArray1 declr Nothing lenexpr)
+    getdeclr ty'
 
   Const    ty' -> getdeclr ty'
   Restrict ty' -> getdeclr ty'
